beb122fcb385f7be719e768ab8320cff0e4a22bd
Beshoy Ibrahim Asham <beshoasham8@gmail.com>
2023-11-28 00:03:01 +0200

    update insert_query function

--- main.py
+++ main.py
@@ -12,13 +12,14 @@ files_name = natsorted(os.listdir('DocumentCollection'))
 
 # Download NLTK resources (you only need to do this once)
 import nltk
+
 nltk.download('punkt')
 nltk.download('stopwords')
 stop_words = set(stopwords.words('english'))
 
-
 # Create a PorterStemmer instance
 porter = PorterStemmer()
+
 
 # Function to take a document and return a list of terms after preprocessing
 def preprocessing(doc):
@@ -33,6 +34,7 @@ def preprocessing(doc):
             prepared_doc.append(stemmed_term)
     return prepared_doc
 
+
 document_of_terms = []
 for files in files_name:
     with open(f'DocumentCollection\{files}', 'r') as f:
@@ -43,31 +45,36 @@ print('Terms after tokenization, removing stop words, and stemming:')
 print(document_of_terms)
 
 # Ensure you apply stemming in the query_input function as well.
-
 ####### positional index #########
 document_number = 0
 positional_index = {}
+
 for document in document_of_terms:
     # For position and term in the tokens.
+    encountered_terms = set()  # Keep track of encountered terms in the current document
+
     for positional, term in enumerate(document):
-        # print(pos, '-->' ,term)
         # If term already exists in the positional index dictionary.
         if term in positional_index:
 
-            # Increment total freq by 1.
-            positional_index[term][0] = positional_index[term][0] + 1
+            # Check if the term has not been encountered in the current document yet.
+            if term not in encountered_terms:
+                # Increment the number of documents the term appears in.
+                positional_index[term][0] = positional_index[term][0] + 1
 
-            # Check if the term has existed in that DocID before.
-            if document_number in positional_index[term][1]:
-                positional_index[term][1][document_number].append(positional)
-
-            else:
+                # Add the current document to the postings list.
                 positional_index[term][1][document_number] = [positional]
 
-        # If term does not exist in the positional index dictionary
-        # (first encounter).
-        else:
+                # Mark the term as encountered in the current document.
+                encountered_terms.add(term)
 
+            else:
+                # If the term has already been encountered in the current document,
+                # append the current position to the existing list.
+                positional_index[term][1][document_number].append(positional)
+
+        # If term does not exist in the positional index dictionary (first encounter).
+        else:
             # Initialize the list.
             positional_index[term] = []
             # The total frequency is 1.
@@ -77,6 +84,9 @@ for document in document_of_terms:
             # Add doc ID to postings list.
             positional_index[term][1][document_number] = [positional]
 
+            # Mark the term as encountered in the current document.
+            encountered_terms.add(term)
+
     # Increment the file no. counter for document ID mapping
     document_number += 1
 
@@ -84,7 +94,7 @@ print('Positional index')
 print(positional_index)
 
 ### phrase Query ###
-query = input('Input Phrase Query: ')
+# query = input('Input Phrase Query: ')
 
 def query_input(q):
     lis = [[] for _ in range(10)]
@@ -108,13 +118,16 @@ def query_input(q):
             positions.append(f'doc{pos}')
 
     return positions
-print(query_input(query))
 
-######### Print Tabels before input Query #############
+
+# print(query_input(query))
+
+######### Print Tables before input Query #############
 all_words = []
 for doc in document_of_terms:
     for word in doc:
         all_words.append(word)
+
 
 def get_term_freq(doc):
     words_found = dict.fromkeys(all_words, 0)
@@ -123,58 +136,64 @@ def get_term_freq(doc):
     return words_found
 
 
-term_freq = pd.DataFrame(get_term_freq(document_of_terms[0]).values(), index=get_term_freq(document_of_terms[0]).keys())
+term_freq = pd.DataFrame(index=get_term_freq(document_of_terms[0]).keys())
 
-for i in range(1, len(document_of_terms)):
+for i in range(0, len(document_of_terms)):
     term_freq[i] = get_term_freq(document_of_terms[i]).values()
 
-term_freq.columns = ['doc ' + str(i) for i in range (1,11)]
+term_freq.columns = ['doc ' + str(i) for i in range(1, 11)]
 print('TF')
 print(term_freq)
 
 
+##################################################################################################################
 def get_weighted_term_freq(x):
     if x > 0:
         return math.log10(x) + 1
     return 0
 
 
+weighted_term_freq = pd.DataFrame(index=get_term_freq(document_of_terms[0]).keys())
+
 for i in range(1, len(document_of_terms) + 1):
-    term_freq['doc ' + str(i)] = term_freq['doc ' + str(i)].apply(get_weighted_term_freq)
+    weighted_term_freq[i] = term_freq['doc ' + str(i)].apply(get_weighted_term_freq)
+weighted_term_freq.columns = ['doc ' + str(i) for i in range(1, 11)]
 
 print('Weighted TF')
-print(term_freq)
-
-
-tfd = pd.DataFrame(columns=['freq', 'idf'])
+print(weighted_term_freq)
+######################################################################################################################
+DF_IDF = pd.DataFrame(columns=['df', 'idf'])
 
 for i in range(len(term_freq)):
-    frequency = term_freq.iloc[i].values.sum()
+    # Count the number of documents the term appears in (non-zero entries in the column).
+    frequency = np.count_nonzero(term_freq.iloc[i].values)
 
-    tfd.loc[i, 'freq'] = frequency
+    DF_IDF.loc[i, 'df'] = frequency
 
-    tfd.loc[i, 'idf'] = math.log10(10 / (float(frequency)))
+    DF_IDF.loc[i, 'idf'] = math.log10(len(document_of_terms) / (float(frequency)))
 
-tfd.index = term_freq.index
-
+DF_IDF.index = term_freq.index
 print('IDF')
-print(tfd)
+print(DF_IDF)
 
-term_freq_inve_doc_freq = term_freq.multiply(tfd['idf'], axis=0)
 
+term_freq_inve_doc_freq = term_freq.multiply(DF_IDF['idf'], axis=0)
 print('TF.IDF')
 print(term_freq_inve_doc_freq)
 
-
 document_length = pd.DataFrame()
+
+##############################################################################
 def get_docs_length(col):
     return np.sqrt(term_freq_inve_doc_freq[col].apply(lambda x: x ** 2).sum())
+
+
 for column in term_freq_inve_doc_freq.columns:
     document_length.loc[0, column + '_len'] = get_docs_length(column)
 
 print('Document Length')
 print(document_length)
-
+###############################################################################################
 normalized_term_freq_idf = pd.DataFrame()
 
 
@@ -193,47 +212,77 @@ print(normalized_term_freq_idf)
 
 
 ######## input Query ##########
+# Function to get the total frequency of a term across all documents
+def get_term_frequency(term):
+    return term_freq.loc[term].sum()
 
 def insert_query(q):
+    terms_in_documents = set(normalized_term_freq_idf.index)
 
-        query = pd.DataFrame(index=normalized_term_freq_idf.index)
-        query['tf'] = [1 if x in preprocessing(q) else 0 for x in list(normalized_term_freq_idf.index)]
-        query['w_tf'] = query['tf'].apply(lambda x: get_weighted_term_freq(x))
-        product = normalized_term_freq_idf.multiply(query['w_tf'], axis=0)
-        query['idf'] = tfd['idf'] * query['w_tf']
-        query['tf_idf'] = query['w_tf'] * query['idf']
-        query['normalized'] = 0
-        for i in range(len(query)):
-            query['normalized'].iloc[i] = float(query['idf'].iloc[i]) / math.sqrt(sum(query['idf'].values ** 2))
-        print('Query Details')
-        print(query.loc[preprocessing(q)])
-        product2 = product.multiply(query['normalized'], axis=0)
-        scores = {}
-        for col in product2.columns:
-            if 0 in product2[col].loc[preprocessing(q)].values:
-                pass
-            else:
-                scores[col] = product2[col].sum()
-        product_result = product2[list(scores.keys())].loc[preprocessing(q)]
-        print()
-        print('Product (query*matched doc)')
-        print(product_result)
-        print()
-        print('product sum')
-        print(product_result.sum())
-        print()
-        print('Query Length')
-        q_len = math.sqrt(sum([x ** 2 for x in query['idf'].loc[preprocessing(q)]]))
-        print(q_len)
-        print()
-        print('Cosine Simliarity')
-        print(product_result.sum())
-        print()
-        sorted_scores = sorted(scores.items(), key=lambda x: x[1], reverse=True)
-        print('Returned docs')
-        for typle in sorted_scores:
-            print(typle[0])
+    # Tokenize, remove stop words, and stem the query terms
+    query_terms = preprocessing(q)
+
+    # Count the occurrences of each term in the query
+    term_counts = {}
+    for term in query_terms:
+        term_counts[term] = term_counts.get(term, 0) + 1
+
+    # Check if each query term exists in the documents
+    for term, count in term_counts.items():
+        if term in terms_in_documents:
+            total_frequency = get_term_frequency(term)
+            print(f'Term "{term}" exists in the query {count} times and in the documents {total_frequency} times.')
+        else:
+            print(f'Term "{term}" does not exist in the documents.')
+
+    # Filter out non-existing terms from the query
+    existing_query_terms = [term for term in term_counts.keys() if term in terms_in_documents]
+
+    if not existing_query_terms:
+        print('No valid terms found in the query. Exiting...')
+        return
+
+    # Further processing for existing query terms
+    query = pd.DataFrame(index=normalized_term_freq_idf.index)
+    query['tf'] = [term_counts.get(x, 0) for x in query.index]
+    query['w_tf'] = query['tf'].apply(lambda x: get_weighted_term_freq(x))
+    product = normalized_term_freq_idf.multiply(query['w_tf'], axis=0)
+    query['idf'] = DF_IDF['idf'] * query['w_tf']
+    query['tf_idf'] = query['w_tf'] * query['idf']
+    query['normalized'] = query['idf'] / np.sqrt((query['idf'] ** 2).sum())
+
+    print('Query Details:')
+    print(query.loc[existing_query_terms])
+
+    product2 = product.multiply(query['normalized'], axis=0)
+    scores = {}
+    for col in product2.columns:
+        if 0 in product2[col].loc[existing_query_terms].values:
+            pass
+        else:
+            scores[col] = product2[col].sum()
+
+    if not scores:
+        print('No matching documents found.')
+        return
+
+    product_result = product2[list(scores.keys())].loc[existing_query_terms]
+
+    print('\nProduct (query * matched doc):')
+    print(product_result)
+    print('\nProduct sum:')
+    print(product_result.sum())
+    print('\nQuery Length:')
+    q_len = math.sqrt(sum([x ** 2 for x in query['idf'].loc[existing_query_terms]]))
+    print(q_len)
+    print('\nCosine Similarity:')
+    print(product_result.sum())
+    print('\nReturned docs:')
+
+    sorted_scores = sorted(scores.items(), key=lambda x: x[1], reverse=True)
+    for doc_id, score in sorted_scores:
+        print(f'Doc {doc_id} - Score: {score}')
 
 
-q = input('Input Query for print Query details and matched document')
+q = input('Input Query for print Query details and matched document: ')
 insert_query(q)
